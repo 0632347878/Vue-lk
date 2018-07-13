45be6015aa4f9f89778587589dd86c0abddc0171
vyshkevich <denis.vyshkevich@brander.ua>
2018-07-13 19:48:19 +0300

    ES6 classes, Tanks

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log("true");
+class Tank {
+    constructor(ammunition) {
+        this.ammunition = ammunition;
+    }
+
+    fireTo() {
+        //Перед выстрелом проверяем снаряды:
+        if (this.canFire(this.ammunition)) {
+            this.ammunition = this.ammunition - 1;
+        }
+    }
+
+    //Вспомогательный метод для проверки снарядов:
+    canFire(ammunition) {
+        if (ammunition > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+var tank = new Tank(3);
+
+alert(tank.ammunition);
+tank.fireTo();
+alert(tank.ammunition);
+tank.fireTo();
+alert(tank.ammunition);
+tank.fireTo();
+alert(tank.ammunition);
+tank.fireTo();
